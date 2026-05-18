1ae52cb4efd7da4459cf8b6688e8cfc7227c6234
KenjiroOgura <kenjiroogura94@gmail.com>
2026-05-18 11:27:08 +0800

    Day One - Blackjack

--- DayOne.java
+++ DayOne.java
@@ -1,5 +1,19 @@
 public class DayOne {
     public static void main(String[] args) {
-        System.out.println("From Eclipse");
+        System.out.println(blackjack(1,2));
+        System.out.println(blackjack(21,22));
+        System.out.println(blackjack(22,22));
+        System.out.println(blackjack(2,10));
+
+    }
+    
+    private static int blackjack(int a, int b) {
+    	if (b>21 && a <=21 && a>0) {
+    		return a;
+    	}
+    	else if(b>0 && b<=21){
+    		return b;
+    	}
+    	return 0;
     }
 }
