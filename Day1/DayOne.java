e58330fa2c4cd3f5061d0dbb1503522c0b761f23
KenjiroOgura <kenjiroogura94@gmail.com>
2026-05-20 10:50:16 +0800

    Separated files

--- Day1/DayOne.java
+++ Day1/DayOne.java
@@ -1,21 +1,20 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-package training;
 public class DayOne {
-//    public static void main(String[] args) {
-////        System.out.println(blackjack(1,2));
-////        System.out.println(blackjack(21,22));
-////        System.out.println(blackjack(22,22));
-////        System.out.println(blackjack(2,10));
-//
+    public static void main(String[] args) {
+//        System.out.println(blackjack(1,2));
+//        System.out.println(blackjack(21,22));
+//        System.out.println(blackjack(22,22));
+//        System.out.println(blackjack(2,10));
+
+        
+    //	zigzag();
 //        
-//    	//zigzag();
-//        
-////        dayOfWeek();
-//    	//pyramid()
-//dayOfWeek2();
-//
-//    }
+       //dayOfWeek();
+//pyramid();
+  	dayOfWeek2();
+
+    }
     
     private static int blackjack(int a, int b) {
     	if (b>21 && a <=21 && a>0 || (a<=21 && b<=21 && a>b)) {
@@ -26,7 +25,7 @@ public class DayOne {
     	}
     	return 0;
     }
-    private static void zigzag() {
+    private static void zigzag(){
 		boolean isvalid= false;
 		int num=0;
 		Scanner sc = new Scanner(System.in);
@@ -68,87 +67,44 @@ public class DayOne {
 	    }
     
     
-    private static void zigzag() {
-    	boolean isvalid= false;
-    	int num=0;
-    	Scanner sc = new Scanner(System.in);
-      
-		System.out.println("Enter a number:");
-        num = sc.nextInt();
-        isvalid = true;
-    
    
-        boolean reverse=false;
-        int max = num*num;
-        int pointer =0;
-        int counter = 0;
-        for(int i=1; i<=max;i++) {
-        	if(reverse) {
-        		for(int j=i+num-1;j>=i;j--) {
-        			System.out.print(j+" ");
-        		}
-        		reverse = false;
-        		i+=num-1;
-        		System.out.println();
-        		counter =0;
-        	}
-        	else {
-        		System.out.print(i+" ");
-        		counter++;
-        		if(counter==num) {
-        			System.out.println();
-        			counter =0;
-        			reverse = true;
-        	}
-        		
-        		}
-        	}
-
-        	
-        	sc.close();
-        	
-        }
     
     	private static void dayOfWeek() {
-    		boolean isvalid=false;
     		Scanner sc = new Scanner(System.in);
     		int num = -1;
     		
-    		do {
+    		
     			System.out.println("Enter a number 1 to 7:");
     			num = sc.nextInt();
     			switch(num) {
 	    			case 1:
 	    				System.out.println("Monday");
-	    				isvalid = true;
+	    				break;
 	    			case 2:
 	    				System.out.println("Tuesday");
-	    				isvalid = true;
-	    				;
+	    				break;
 	    			case 3:
 	    				System.out.println("Wednesday");
-	    				isvalid = true;
+	    				break;
 	    				
 	    			case 4:
 	    				System.out.println("Thursday");
-	    				isvalid = true;
+	    				break;
 	    			case 5:
 	    				System.out.println("Friday");
-	    				isvalid = true;
+	    				break;
 
 	    			case 6:
 	    				System.out.println("Saturday");
-	    				isvalid = true;
+	    				break;
 	    			case 7:
 	    				System.out.println("Sunday");
-	    				isvalid = true;
-	    				
+	    				break;
     				default:
     					System.out.println("Invalid day number");
-    					continue;
+
     			}
     					
-    		}while(!isvalid);
     		
     		sc.close();
     	}
@@ -218,7 +174,6 @@ public class DayOne {
     				num = sc.nextInt();
     				if(num<21 && num >0) {
     					isvalid = true;
-        				System.out.println("valid");
     				}else {
     					System.out.println("Error: Enter numbers between 1 to 20");
     				}
@@ -238,6 +193,7 @@ public class DayOne {
     		}
     		
     	}
+    	
     }
     
     
