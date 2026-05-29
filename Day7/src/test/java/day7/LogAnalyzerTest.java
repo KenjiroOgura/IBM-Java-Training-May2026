d21ba472cb80e1781e2a8483ae29225f8c9c9263
KenjiroOgura <kenjiroogura94@gmail.com>
2026-05-29 15:38:06 +0800

    Changed folder names

--- Day7/src/test/java/day7/LogAnalyzerTest.java
+++ Day7/src/test/java/day7/LogAnalyzerTest.java
@@ -1,4 +1,4 @@
-package test.java.day7;
+package day7;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -29,7 +29,7 @@ class LogAnalyzerTest extends LogAnalyzer {
 	@BeforeEach
 	void setUp() throws Exception {
 		LogAnalyzer log = new LogAnalyzer();
-		File myObj = new File("resources/summary.txt");
+		File myObj = new File("src/main/resources/summary.txt");
 		myObj.delete();
 	}
 
@@ -40,16 +40,16 @@ class LogAnalyzerTest extends LogAnalyzer {
 	        
 	        PrintStream originalOut = System.out;
 
-	        String expected = Files.readString(Path.of("src/resources/MssgMissngTest/missingSummary.txt"));
+	        String expected = Files.readString(Path.of("src/test/resources/Exec003/missingSummary.txt"));
 	        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 	        System.setOut(new PrintStream(outputStream));
-	        String args[] = {"src/resources/MssgMissngTest/Test2.log"};
+	        String args[] = {"src/test/resources/Exec003/Test2.log"};
 	        try {
 	            LogAnalyzer.main(args);
-	            String actual = Files.readString(Path.of("src/resources/summary.txt"));
+	            String actual = Files.readString(Path.of("src/main/resources/summary.txt"));
 	            String output = outputStream.toString();
 	            assertEquals("Skipping malformed line: [2024-05-10 09:00:06] \r\n"
-	            		+ "Analysis complete. Summary written to summary.txt", output);
+	            		+ "Analysis complete. Summary written to summary.txt\r\n", output);
 	            assertEquals(expected, actual);
 	        } finally {
 	            System.setOut(originalOut);
@@ -58,39 +58,39 @@ class LogAnalyzerTest extends LogAnalyzer {
 
 	@Test
     void testMainThrowsMalformedException() throws Exception {
-		PrintStream originalOut = System.out;
+        PrintStream originalOut = System.out;
 
-        String expected = Files.readString(Path.of("src/resources/MalformedExceptionTest/malformed_summary.txt"));
+        String expected = Files.readString(Path.of("src/test/resources/Exec002/malformed_summary.txt"));
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
-        String args[] = {"src/resources/MalformedExceptionTest/Test1.log"};
+        String args[] = {"src/test/resources/Exec002/Test1.log"};
         try {
             LogAnalyzer.main(args);
-            String actual = Files.readString(Path.of("src/resources/summary.txt"));
+            String actual = Files.readString(Path.of("src/main/resources/summary.txt"));
             String output = outputStream.toString();
+        
             assertEquals("Skipping malformed line: 2024-05-10 09:00:06] INFO: Database connection established\r\n"
-            		+ "Skipping malformed line: [2024-05-10 09:00:21 INFO: Backup completed successfully\r\n"
-            		+ "Analysis complete. Summary written to summary.txt", output);
+          		+ "Skipping malformed line: [2024-05-10 09:00:21 INFO: Backup completed successfully\r\n"
+          		+ "Analysis complete. Summary written to summary.txt\r\n", output);
             assertEquals(expected, actual);
-        } finally {
-            System.setOut(originalOut);
-        }
-    }
-
+	    }finally {
+	        System.setOut(originalOut);
+	    }
+	}
 	@Test
     void testTypeMissing() throws Exception {
 		PrintStream originalOut = System.out;
 
-        String expected = Files.readString(Path.of("src/resources/TypeMissingTest/summary_type.txt"));
+        String expected = Files.readString(Path.of("src/test/resources/Exec004/summary_type.txt"));
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
-        String args[] = {"src/resources/TypeMissingTest/Test3.log"};
+        String args[] = {"src/test/resources/Exec004/Test3.log"};
         try {
             LogAnalyzer.main(args);
-            String actual = Files.readString(Path.of("src/resources/summary.txt"));
+            String actual = Files.readString(Path.of("src/main/resources/summary.txt"));
             String output = outputStream.toString();
             assertEquals("Skipping malformed line: [2024-05-10 09:00:24] : Failed to connect to external API\r\n"
-            		+ "Analysis complete. Summary written to summary.txt", output);
+            		+ "Analysis complete. Summary written to summary.txt\r\n", output);
             assertEquals(expected, actual);
         } finally {
             System.setOut(originalOut);
@@ -124,16 +124,16 @@ class LogAnalyzerTest extends LogAnalyzer {
 
         PrintStream originalOut = System.out;
 
-        String expected = Files.readString(Path.of("src/resources/MainTest/summary_testMain.txt"));
+        String expected = Files.readString(Path.of("src/test/resources/Exec001/summary_testMain.txt"));
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
-        String args[] = {"src/resources/server.log"};
+        String args[] = {"src/main/resources/server.log"};
         try {
             LogAnalyzer.main(args);
-            String actual = Files.readString(Path.of("src/resources/summary.txt"));
+            String actual = Files.readString(Path.of("src/main/resources/summary.txt"));
             String output = outputStream.toString();
 
-            assertEquals("Analysis complete. Summary written to summary.txt", output);
+            assertEquals("Analysis complete. Summary written to summary.txt\r\n", output);
             assertEquals(expected, actual);
         } finally {
             System.setOut(originalOut);
@@ -146,55 +146,24 @@ class LogAnalyzerTest extends LogAnalyzer {
 	@Test
     void shouldCreateFile() throws IOException {
 		
-		String args[] = {"src/resources/Server.log"};
+		String args[] = {"src/main/resources/Server.log"};
 		LogAnalyzer.main(args);
-		Path path = Paths.get("src/resources/summary.txt");
+		Path path = Paths.get("src/main/resources/summary.txt");
 	    assertTrue(Files.exists(path), "File should exist");
         
-        
+   
     }
 	
-//	@Test
-//	void testFileContent() throws Exception {
-//	    Path filePath = Path.of("resources/summary.txt");
-//	    String content = Files.readString(filePath);
-////	    String expected = "Log Summary Report\r\n------------------\r\n"
-////	    		+ "Total Entries: 85\r\n"
-////	    		+ "INFO: 66\r\n"
-////	    		+ "WARN: 10\r\n"
-////	    		+ "ERROR: 9\r\n"
-////	    		+ "\r\n"
-////	    		+ "Error Messages:\r\n"
-////	    		+ "- Failed to connect to external API\r\n"
-////	    		+ "- NullPointerException in UserService.java line 87\r\n"
-////	    		+ "- Timeout while reading data from database\r\n"
-////	    		+ "- FileNotFoundException in ConfigLoader.java line 45\r\n"
-////	    		+ "- Unauthorized access attempt detected\r\n"
-////	    		+ "- Assertion failed in TestCase #45\r\n"
-////	    		+ "- Unable to parse JSON response from API\r\n"
-////	    		+ "- SMTP server not responding\r\n"
-////	    		+ "- Audit log write failure\r\n"
-////	    		+ "\r\n"
-////	    		+ "Earliest Timestamp: 2024-05-10T09:00\r\n"
-////	    		+ "Latest Timestamp: 2024-05-10T09:04:18";
-//	    String expected = Files.readString(Path.of("resouces/summary_testFileContent.txt"));
-////		expected = expected.replace("\r\n", "\n");
-////		content = content.replace("\r\n", "\n");
-//		
-//		assertEquals(expected.trim(), content.trim());
-//
-//	    assertEquals(expected, content.trim());
-//	}
 
 	@Test
 	void testIO() throws IOException {
 
         PrintStream originalOut = System.out;
-        String args[] = {"src/resources/test5.log"};
+        String args[] = {"src/test/resources/test5.log"};
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
-		try (FileOutputStream fos = new FileOutputStream("src/resources/test5.log");
+		try (FileOutputStream fos = new FileOutputStream("src/test/resources/test5.log");
 				FileChannel channel = fos.getChannel()) {
 	           
 	           FileLock lock = channel.lock();
@@ -218,15 +187,15 @@ class LogAnalyzerTest extends LogAnalyzer {
 	@Test
 	void testWrite() throws FileNotFoundException, IOException {
 		 
-		      File myObj = new File("src/resources/summary.txt");
+		      File myObj = new File("src/main/resources/summary.txt");
 		      myObj.createNewFile();           
 		        
 			 	PrintStream originalOut = System.out;
-		        String args[] = {"src/resources/server.log"};
+		        String args[] = {"src/main/resources/server.log"};
 	
 		        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		        System.setOut(new PrintStream(outputStream));
-			try (FileOutputStream fos = new FileOutputStream("src/resources/summary.txt");
+			try (FileOutputStream fos = new FileOutputStream("src/main/resources/summary.txt");
 					FileChannel channel = fos.getChannel()) {
 		           
 		           FileLock lock = channel.lock();
